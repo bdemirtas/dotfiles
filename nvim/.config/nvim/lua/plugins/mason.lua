b292bd7e372edb864227bc7f2830640cc29d86ba
Burak Demirtas <burak@burakdemirtas.io>
2024-05-15 19:24:06 -0400

    revert back nonels, doesn't work well

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,46 +1,54 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-      })
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = {
-      "williamboman/mason.nvim"
-    },
-    config = function()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          -- LSP
-          "typescript-language-server",
-          "eslint-lsp",
-          "pyright",
-          "lua_ls",
-          "marksman",
-          "yamlls",
-          "bashls",
-          "dockerls",
-          "jsonls",
-          "html",
-          "jqls",
-          "jsonls",
-          "marksman",
-          "cssls",
-          "taplo",
-          "ruff_lsp"
-        },
-      })
-    end
-  }
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			})
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					-- LSP
+					"eslint-lsp",
+					"pyright",
+					"lua_ls",
+					"marksman",
+					"yamlls",
+					"bashls",
+					"dockerls",
+					"jsonls",
+					"html",
+					"jqls",
+					"marksman",
+					"cssls",
+					"taplo",
+					"ruff_lsp",
+					"prettierd",
+					-- Formatter
+					"eslint_d",
+					"prettier",
+					"fixjson",
+					"jq",
+					"ruff",
+					"yamlfmt",
+					"markdownlint",
+					"stylua",
+				},
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,9 +1,37 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      enable_git_status = true,
+      enable_diagnostics = true,
+      icon = {
+        folder_closed = "",
+        folder_open = "",
+        folder_empty = "󰜌",
+        default = "*",
+        highlight = "NeoTreeFileIcon"
+      },
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "✚",
+          deleted = "✖",
+          modified = "",
+          renamed = "",
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "",
+          staged = "",
+          conflict = "",
+        },
+      },
+    })
+  end
 }
