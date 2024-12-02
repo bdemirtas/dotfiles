bc2b1a268ebd9356db15f4eb3b568fc4c0fc458d
Burak Demirtas <burak@burakdemirtas.io>
2024-12-02 09:16:07 -0500

    refact: merge plugins in one file

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -2,7 +2,9 @@ return {
   {
     "williamboman/mason.nvim",
     lazy = false,
-    config = true,
+    opts = {
+      max_concurrent_installers = 10,
+    },
   },
   {
     "williamboman/mason-lspconfig.nvim",
@@ -13,6 +15,7 @@ return {
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    lazy = false,
     opts = function()
       return require "configs.mason"
     end,
@@ -55,5 +58,77 @@ return {
         python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
       },
     },
+  },
+  {
+    "echasnovski/mini.diff",
+    version = false,
+    config = function()
+      require("mini.diff").setup()
+    end,
+  },
+  {
+    "echasnovski/mini.indentscope",
+    version = false,
+    config = function()
+      require("mini.indentscope").setup {
+        symbol = "|",
+      }
+    end,
+  },
+  {
+    "echasnovski/mini.surround",
+    version = false,
+    config = function()
+      require("mini.surround").setup {}
+    end,
+  },
+  {
+    "echasnovski/mini.pairs",
+    version = false,
+    config = function()
+      require("mini.pairs").setup {}
+    end,
+  },
+  {
+    "echasnovski/mini.ai",
+    version = false,
+    config = function()
+      require("mini.ai").setup {}
+    end,
+  },
+  {
+    "echasnovski/mini.comment",
+    version = false,
+    config = function()
+      require("mini.comment").setup {}
+    end,
+  },
+  {
+    "echasnovski/mini.align",
+    version = false,
+    config = function()
+      require("mini.align").setup {}
+    end,
+  },
+  {
+    "echasnovski/mini.move",
+    config = function()
+      require("mini.move").setup {}
+    end,
+  },
+  {
+    "stevearc/dressing.nvim",
+    opts = {
+      input = {
+        start_in_insert = false,
+        insert_only = false,
+      },
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup {}
+    end,
   },
 }
